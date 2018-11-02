1bdab2f65810b33496823939898f2979beaec72e
Ernest Dominick Mauricio <EMauricio1102@mail.fresnostate.edu>
2018-11-01 21:05:49 -0700

    Added the Prefab for BossRoom and Spawn Room
    Included in this update:
    -All the variations of Boss room, Prefabbed
    -All the variations of Spawn room, Prefabbed
    -Updated scaling of the boss room.

---
+++ Legacy/Assets/Scripts/NewGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour {
+
+    public void BeginGame(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
