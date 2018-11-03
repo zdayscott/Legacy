f6d544580ef5b02de539797d636758b8636e4bdc
Zach <zdayscott@gmail.com>
2018-11-03 16:25:22 -0700

    Player and Enemy scripting
    1) Refactored the PlayerController Script to modularize it for easier usability and scalability.
    2) Made the player into a prefab
    3) Added an Enemy prefab with seek and die ability.
    3a) The current scripts are very basic and will need to be redone later. They are expected to only be used for testing the playerController.
    4) Restructured the Prefab folders
    5) Completely ignored Blockchain system in hopes that it will work itself out

---
+++ Legacy/Assets/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+    public int damage = 5;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            collision.GetComponent<EnemyStats>().TakeDamage(damage);
+        }
+    }
+}
--- Legacy/Assets/Scripts/PlayerController.cs
+++ Legacy/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,14 @@ public class PlayerController : MonoBehaviour {
     public int speed = 5;
 
     [Header("Attack Variables")]
-    public GameObject Attack;
+    //The GameObj that containes the collider and attack sprite
+    public GameObject attackObj;
     Vector2 attackingDirection = new Vector2(1,0);
+
     //Time the attack collider will be active
     public float attackTime = .25f;
     private float attackTimeCurrent;
+
     //Time between attacks
     public float rechargeTime = .4f;
     private float rechargeTimeCurrent;
@@ -30,8 +33,21 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        MovePlayer();
 
+        if (Input.GetButton("Fire1"))
+        {
+            Attack();
+        }
+
+        UpdateCooldowns();
+    }
+
+
+    void MovePlayer()
+    {
         Vector2 move;
         float horz = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
@@ -39,26 +55,31 @@ public class PlayerController : MonoBehaviour {
 
         rb.transform.Translate(move * speed * Time.deltaTime);
 
+        /*if (move.x != 0 || move.y != 0)
+         {
+             attackingDirection = move;
+         }*/
 
+    }
 
-       if (move.x != 0 || move.y != 0)
+    void Attack()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        attackingDirection = new Vector2(mousePos.x - this.transform.position.x, mousePos.y - this.transform.position.y).normalized;
+        if (rechargeTimeCurrent <= 0)
         {
-            attackingDirection = move;
+            attackObj.transform.localPosition = attackingDirection * .7f;
+            attackObj.SetActive(true);
+            attackTimeCurrent = attackTime;
+            rechargeTimeCurrent = rechargeTime;
+            Debug.Log("Firing");
         }
+    }
 
-        if(Input.GetButton("Fire1"))
-        {
-            
-            if (rechargeTimeCurrent <= 0)
-            {
-                Attack.transform.localPosition = attackingDirection * .7f;
-                Attack.SetActive(true);
-                attackTimeCurrent = attackTime;
-                rechargeTimeCurrent = rechargeTime;
-                Debug.Log("Firing");
-            }
-            
-        }
+    void UpdateCooldowns()
+    {
         rechargeTimeCurrent -= Time.deltaTime;
         if(attackTimeCurrent > 0)
         {
@@ -66,12 +87,8 @@ public class PlayerController : MonoBehaviour {
         }
         else
         {
-            Attack.SetActive(false);
+            attackObj.SetActive(false);
 
         }
-
-
-        
-		
-	}
+    }
 }
