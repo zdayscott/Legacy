080cf60bcc8ad9f7863cf160a656f947d699bb84
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-11-04 00:58:58 -0700

    Update FaceMouse.cs

--- Legacy/Assets/FaceMouse.cs
+++ Legacy/Assets/FaceMouse.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class FaceMouse : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	void Update () 
+	{
+           ChangeDirection();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void ChangeDirection()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        Debug.Log(direction);
+        this.transform.up = direction;
+    }
 }
