7ffc144351e943307523dd08acd1ca2b35b08b62
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-11-04 00:47:12 -0700

    Add files via upload

---
+++ Legacy/Assets/EnemyAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public int damage = 5;
+
+    void OnTriggerEnter2D(Collider2D PlayerHit)
+    {
+        if (PlayerHit.gameObject.tag == "Player")
+        {
+            PlayerHit.GetComponent<Player>().takeDamage(damage);
+            Debug.Log("Enemy hit for damage");
+        }
+    }
+}
