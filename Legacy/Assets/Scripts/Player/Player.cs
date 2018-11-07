6fb519940aabf52d7541f97a2fb41806ba179192
bduinkerk <43049255+bduinkerk@users.noreply.github.com>
2018-11-06 16:15:29 -0800

    fixed healthbar

--- Legacy/Assets/Scripts/Player/Player.cs
+++ Legacy/Assets/Scripts/Player/Player.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
-
-    public float health;
-
+    [SerializeField]
+    public float maxHealth=100f;
+    private float health;
+    public Slider healthbar;
 	// Use this for initialization
 	void Start ()
     {
-        health = 100;
+        health = maxHealth;
+        healthbar.maxValue = maxHealth;
+        healthbar.minValue = 0f;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-     
+        
 	}
+
+    private void FixedUpdate()
+    {
+        healthbar.value = health;
+    }
 
     public void takeDamage(int damage)
     {
