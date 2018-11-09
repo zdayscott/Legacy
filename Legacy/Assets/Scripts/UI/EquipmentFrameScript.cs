e17d86eeea8bf80f245b1e8913c9ca3c4c72fd41
bduinkerk <43049255+bduinkerk@users.noreply.github.com>
2018-11-08 17:14:23 -0800

    added EquipmentFrame

---
+++ Legacy/Assets/Scripts/UI/EquipmentFrameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentFrameScript : MonoBehaviour
+{
+
+    public GameObject EquipmentPanel;
+
+    public void OpenMenu()
+    {
+        EquipmentPanel.SetActive(true);
+    }
+    public void CloseMenu()
+    {
+
+        EquipmentPanel.SetActive(false);
+
+    }
+
+
+}
