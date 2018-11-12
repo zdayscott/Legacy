5d2a3dd10e128fc3a435098e3465834af28301f1
bduinkerk <43049255+bduinkerk@users.noreply.github.com>
2018-11-11 23:42:58 -0800

    fixed

--- Legacy/Assets/Scripts/UI/EquipmentFrameScript.cs
+++ Legacy/Assets/Scripts/UI/EquipmentFrameScript.cs
@@ -6,17 +6,26 @@ public class EquipmentFrameScript : MonoBehaviour
 {
 
     public GameObject EquipmentPanel;
+    public GameObject EquipmentButtonOpen;
+    public GameObject EquipmentButtonClose;
 
     public void OpenMenu()
     {
         EquipmentPanel.SetActive(true);
+        EquipmentButtonOpen.SetActive(false);
+        EquipmentButtonClose.SetActive(true);
     }
     public void CloseMenu()
     {
-
         EquipmentPanel.SetActive(false);
-
+        EquipmentButtonOpen.SetActive(true);
+        EquipmentButtonClose.SetActive(false);
     }
-
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            EquipmentPanel.gameObject.SetActive(!EquipmentPanel.gameObject.activeSelf);
+        }
+    }
 }
