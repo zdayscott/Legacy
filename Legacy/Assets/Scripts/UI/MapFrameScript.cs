c03ef87531d40b367609f0b69f0032defb906744
bduinkerk <43049255+bduinkerk@users.noreply.github.com>
2018-11-11 17:02:03 -0800

    Updated UI with Button Input support

--- Legacy/Assets/Scripts/UI/MapFrameScript.cs
+++ Legacy/Assets/Scripts/UI/MapFrameScript.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class MapFrameScript : MonoBehaviour {
 
     public GameObject MapPanel;
+    public GameObject MapButtonOpen;
+    public GameObject MapButtonClose;
 
     public void OpenMenu()
     {
         MapPanel.SetActive(true);
+        MapButtonOpen.SetActive(false);
+        MapButtonClose.SetActive(true);
     }
     public void CloseMenu()
     {
         MapPanel.SetActive(false);
+        MapButtonOpen.SetActive(true);
+        MapButtonClose.SetActive(false);
     }
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            MapPanel.gameObject.SetActive(!MapPanel.gameObject.activeSelf);
+        }
+    }
 }
--- Legacy/Assets/Scripts/UI/OptionsFrameScript.cs
+++ Legacy/Assets/Scripts/UI/OptionsFrameScript.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class OptionsFrameScript : MonoBehaviour {
 
     public GameObject OptionsPanel;
+    public GameObject OptionsButtonOpen;
+    public GameObject OptionsButtonClose;
 
     public void OpenMenu()
     {
         OptionsPanel.SetActive(true);
+        OptionsButtonOpen.SetActive(false);
+        OptionsButtonClose.SetActive(true);
     }
     public void CloseMenu()
     {
         OptionsPanel.SetActive(false);
+        OptionsButtonOpen.SetActive(true);
+        OptionsButtonClose.SetActive(false);
     }
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OptionsPanel.gameObject.SetActive(!OptionsPanel.gameObject.activeSelf);
+        }
+    }
 }
