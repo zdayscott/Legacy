18c3294989e2756bc721ebc7673cc285ad2efac6
Ernest Mauricio <EMauricio1102@mail.fresnostate.edu>
2018-11-25 15:50:19 -0800

    Added Base Room Generation
    Implemented spawn points to allow a room to spawn.
    Whats needed:
    - Integrate the scripts on and spawn points to the rest rooms in order to create a more developed level.

---
+++ Legacy/Assets/RoomTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+
+    public GameObject[] leftRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] topRooms;
+
+}
