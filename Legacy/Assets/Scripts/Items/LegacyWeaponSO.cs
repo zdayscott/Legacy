259454ea0c8e0170c4fc2916d190d9ac85086894
Zach <zdayscott@gmail.com>
2018-11-27 13:10:03 -0800

    Legacy Weapon SO
    1) Started Legacy Weapon dev

---
+++ Legacy/Assets/Scripts/Items/LegacyWeaponSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegacyWeaponSO : WeaponSO {
+    public int level;
+
+    public Dictionary<string, int> Kills;
+}
