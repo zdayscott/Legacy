33cbd4a9c58c5604d6844dfef7bb5befbb253385
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-11-29 22:05:50 -0800

    Explode Script
    Used to spread fire when projectiles collide

---
+++ Legacy/Assets/Scripts/Enemy/Explode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+    public int damage;
+    public float timeAlive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(FireDuration());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator FireDuration()
+    {
+        yield return new WaitForSeconds(timeAlive);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.gameObject.tag == "Player")
+        {
+            collide.GetComponent<Player>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        else if (collide.gameObject.tag != "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
