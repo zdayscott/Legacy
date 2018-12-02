7a47fb7c0f52e8986316bffd721027932d97ee30
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-12-01 19:45:28 -0800

    Player can now throw projectiles
    - Fire set set to attack fire rate (0.4f)
    - Player throws his projectile with righ click

--- Legacy/Assets/Scripts/Player/PlayerController.cs
+++ Legacy/Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour {
     [Header("Attack Variables")]
     //The GameObj that containes the collider and attack sprite
     public GameObject attackObj;
+    public GameObject ninjaStar;
     Vector2 attackingDirection = new Vector2(1,0);
 
     //Time the attack collider will be active
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour {
     public float rechargeTime = .4f;
     private float rechargeTimeCurrent;
 
-
+    // Keep track of time until player can throw another projectile
+    private float nextStarReady = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -41,7 +43,10 @@ public class PlayerController : MonoBehaviour {
         {
             Attack();
         }
-
+        if (Input.GetButton("Fire2"))
+        {
+            ThrowProjectile();
+        }
         UpdateCooldowns();
     }
 
@@ -71,9 +76,29 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    void ThrowProjectile()
+    {
+        // calculate direction the player is facing
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        // Calculate which direction the mouse is moving
+        Vector3 attackDir = new Vector3(mousePos.x - this.transform.position.x, mousePos.y - this.transform.position.y, 0).normalized;
+        Vector3 offset = new Vector3(attackDir.x * .3f, attackDir.y * .3f, 0); // offset needed to avoid collision with player
+
+        if (nextStarReady <= 0)
+        {
+            GameObject star = Instantiate(ninjaStar, transform.position + attackDir + offset, gameObject.transform.rotation) as GameObject;
+            Rigidbody2D rbStar = star.GetComponent<Rigidbody2D>();
+            rbStar.AddForce(attackDir * speed, ForceMode2D.Impulse);
+            nextStarReady = rechargeTime;
+        }
+    }
+
     void UpdateCooldowns()
     {
         rechargeTimeCurrent -= Time.deltaTime;
+        nextStarReady -= Time.deltaTime;
         if(attackTimeCurrent > 0)
         {
             attackTimeCurrent -= Time.deltaTime;
