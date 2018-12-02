10fb2fae3c0b2fb2efd76c0a93e806dbaf31b30e
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-12-01 18:01:14 -0800

    Updated Bomb script
    - Bomb now spreads fire in the surrounding area when it explodes
    - Bomb now applies a force in the opposite direction of any object caught in the blast

--- Legacy/Assets/Scripts/Enemy/Bomb.cs
+++ Legacy/Assets/Scripts/Enemy/Bomb.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Bomb : MonoBehaviour {
     // GameObject used for fire sprite after bomb explodes
     public GameObject fire;
+    private GameObject[] fireEmbers = new GameObject[8];
+
     public float timer;
     public float radius = 1.5f;
     public float power = 5.0f;
     public int damage = 30;
+    private bool exploded;
 
     // Colors the bomb will alternate between
     public Color StartColor = Color.black;
     public Color endColor = Color.red;
-
 
     private SpriteRenderer bombSprite;
     public Material bombMaterial;
@@ -22,22 +24,15 @@ public class Bomb : MonoBehaviour {
 	void Start ()
     {
         bombSprite = GetComponent<SpriteRenderer>();
-
-        StartCoroutine(Detonate());
+        exploded = false;
         StartCoroutine(ChangeColor());
+        Invoke("Explode", timer);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
        
-    }
-
-    IEnumerator Detonate()
-    {
-        yield return new WaitForSeconds(timer);
-        Explode();
-        Destroy(gameObject);
     }
 
     IEnumerator ChangeColor()
@@ -57,19 +52,47 @@ public class Bomb : MonoBehaviour {
 
     void Explode()
     {
+        //Destroy(gameObject, 0.13f);
         Collider2D[] colliders;
 
         colliders = Physics2D.OverlapCircleAll(transform.position, radius);
 
+        // Create a circle collider around the bomb
+        // that damages any nearby enemmies and player
         foreach (Collider2D hit in colliders)
         {
+            Rigidbody2D rb = hit.gameObject.GetComponent<Rigidbody2D>();
+
+            // Calculate the current direction the player is moving
+            // Push the player/enemy in the opposite direction from which they moving
+            Vector2 direction = new Vector2(gameObject.transform.position.x - rb.transform.position.x, gameObject.transform.position.y - rb.transform.position.y);
+            rb.AddForce(- direction * power, ForceMode2D.Impulse);
+
             if (hit.gameObject.tag == "Player")
             {
-
-                Rigidbody2D rb = hit.gameObject.GetComponent<Rigidbody2D>();
-                rb.AddForce(transform.up * power, ForceMode2D.Impulse);
                 hit.GetComponent<Player>().TakeDamage(damage);
             }
+
+            if (hit.gameObject.tag == "Enemy")
+            {
+                hit.GetComponent<EnemyStats>().TakeDamage(damage, fire);
+            }
+        }
+
+        // Once the bomb timer runs out, call function
+        // to spread fire in random directions
+        StartCoroutine(SpreadFire());
+        bombSprite.enabled = false;
+        Destroy(gameObject, .16f);
+    }
+
+    private IEnumerator SpreadFire()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Vector3 direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+            Instantiate(fire, transform.position + direction, transform.rotation);
+            yield return new WaitForSeconds(.02f);
         }
     }
 }
