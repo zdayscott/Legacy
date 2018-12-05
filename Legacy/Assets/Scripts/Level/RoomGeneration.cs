8f0471db971eed2bd4a32019286a7ba8cd38bffe
zdayscott <zdayscott@gmail.com>
2018-12-04 16:19:14 -0800

    Fixed RoomGeneration console error
    1) Commented out Debug
    2) Checked if the room generation, component is null

--- Legacy/Assets/Scripts/Level/RoomGeneration.cs
+++ Legacy/Assets/Scripts/Level/RoomGeneration.cs
@@ -19,11 +19,11 @@ public class RoomGeneration : MonoBehaviour {
     void Start()
     {
         Destroy(gameObject, waitTime);
-        Debug.Log("Room Start" + this.gameObject.name);
+        //Debug.Log("Room Start" + this.gameObject.name);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        Debug.Log("Templates Found" + this.gameObject.name);
+        //Debug.Log("Templates Found" + this.gameObject.name);
         Invoke("Spawn", 0.3f);
-        Debug.Log("Spawn Called" + this.gameObject.name);
+        //Debug.Log("Spawn Called" + this.gameObject.name);
     }
 
     void Spawn()
@@ -35,28 +35,28 @@ public class RoomGeneration : MonoBehaviour {
                 rand = Random.Range(0, templates.leftRooms.Length);
                 transform.position = new Vector3(transform.position.x, transform.position.y, 0);
                 Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
-                Debug.Log(transform.position);
+                //Debug.Log(transform.position);
             }
             else if (openingDirection == 2)
             {   //Need a room with bottom door
                 rand = Random.Range(0, templates.bottomRooms.Length);
                 transform.position = new Vector3(transform.position.x, transform.position.y, 0);
                 Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
-                Debug.Log(transform.position);
+                //Debug.Log(transform.position);
             }
             else if (openingDirection == 3)
             {   //Need a room with right door
                 rand = Random.Range(0, templates.rightRooms.Length);
                 transform.position = new Vector3(transform.position.x, transform.position.y, 0);
                 Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-                Debug.Log(transform.position);
+                //Debug.Log(transform.position);
             }
             else if (openingDirection == 4)
             {   //Need a room with top door
                 rand = Random.Range(0, templates.topRooms.Length);
                 transform.position = new Vector3(transform.position.x, transform.position.y, 0);
                 Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
-                Debug.Log(transform.position);
+                //Debug.Log(transform.position);
             }
             spawned = true; 
         }
@@ -65,6 +65,12 @@ public class RoomGeneration : MonoBehaviour {
     {
         if (collider.CompareTag("RoomSpawn"))
         {
+            if(collider.GetComponent<RoomGeneration>() == null)
+            {
+                //Debug.Log("RoomG is null");
+                return;
+            }
+
             if(collider.GetComponent<RoomGeneration>().spawned == false && spawned == false)
             {
                 Destroy(gameObject);
