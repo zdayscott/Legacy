6868d5f6546ac6b0e3d06e104336cd4c771f726e
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2018-12-05 16:31:56 -0800

    Updated explode script
    - All fire created will now alternate in color between red and yellow

--- Legacy/Assets/Scripts/Enemy/Explode.cs
+++ Legacy/Assets/Scripts/Enemy/Explode.cs
@@ -7,11 +7,19 @@ public class Explode : MonoBehaviour {
     public int damage;
     public float timeAlive;
 
-	// Use this for initialization
-	void Start ()
+    private SpriteRenderer fireSprite;
+
+    // Colors the fire will alternate between
+    public Color StartColor = Color.red;
+    public Color endColor = Color.yellow;
+
+    // Use this for initialization
+    void Start ()
     {
+        fireSprite = GetComponent<SpriteRenderer>();
         StartCoroutine(FireDuration());
-	}
+        StartCoroutine(ChangeColor());
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,6 +31,21 @@ public class Explode : MonoBehaviour {
     {
         yield return new WaitForSeconds(timeAlive);
         Destroy(gameObject);
+    }
+
+    IEnumerator ChangeColor()
+    {
+        // Alternate bewteen the color black and red every 1 second
+        float timeElapsed = 0f;
+        float delay = 1f;
+
+        while (timeElapsed < timeAlive)
+        {
+            timeElapsed += Time.deltaTime;
+
+            fireSprite.color = Color.Lerp(endColor, StartColor, Mathf.PingPong(Time.time, timeAlive / timeElapsed));
+            yield return null;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collide)
