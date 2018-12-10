caa7761dca454ffd6779cc84273ac720741cdffe
Ernest Mauricio <EMauricio1102@mail.fresnostate.edu>
2018-12-09 22:59:10 -0800

    Adding comments to the AddRoom script.

--- Legacy/Assets/Scripts/Level/AddRoom.cs
+++ Legacy/Assets/Scripts/Level/AddRoom.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class AddRoom : MonoBehaviour {
 
     private RoomTemplates templates;
+    //To access the rooms from Room Templates
 
     void Start()
     {
+        //To find the rooms with the tag "Rooms"
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        //To find all the rooms that were spawned onto the scene
         templates.rooms.Add(this.gameObject);
     }
 }
