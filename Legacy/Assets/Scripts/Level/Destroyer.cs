6784f17f553e12edad666a94ed5be7fecac93e3e
Ernest Mauricio <EMauricio1102@mail.fresnostate.edu>
2018-12-09 23:03:04 -0800

    Added comments for the Destroyer script.

--- Legacy/Assets/Scripts/Level/Destroyer.cs
+++ Legacy/Assets/Scripts/Level/Destroyer.cs
@@ -7,8 +7,10 @@ public class Destroyer : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("RoomSpawn"))
-        {
+        {   //Checks to see if any rooms spawn on top of the spawn room.
+
             Destroy(collision.gameObject);
+            //Destroys the room ontop of the spawn room.
         }
             
     }
